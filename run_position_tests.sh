7780a31d9ffcbec474fd7eb9e14116589016d12e
Phani Ganti <phaniganti@192-168-1-105.tpgi.com.au>
2016-09-05 21:18:16 +1000

    Changed the production rb file to include config assets to true

--- run_position_tests.sh
+++ run_position_tests.sh
@@ -1,5 +1,5 @@
 ruby testscript-user1.rb &
 ruby testscript-user2.rb &
-# ruby testscript-user3.rb &
-# ruby testscript-user4.rb &
-# ruby testscript-user5.rb &
+ruby testscript-user3.rb &
+ruby testscript-user4.rb &
+ruby testscript-user5.rb &
